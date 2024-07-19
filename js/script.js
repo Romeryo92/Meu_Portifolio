076b4e53458b06483ffa537097360b8e01e2aeb0
Romeryo Lenilson <104010203+Romeryo92@users.noreply.github.com>
2024-07-19 15:00:39 -0300

    Meu Portifolio

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+let btn = document.querySelector(".header_btn");
+let navBar = document.querySelector(".nav_icon");
+let navFa_bars = document.querySelector(".fa-bars");
+let navFa_xmark = document.querySelector(".fa-xmark");
+let color_img = document.querySelector(".my_img");
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle("dark")
+    document.body.classList.toggle("change_color")
+
+    if(document.body.classList == "change_color") {
+        color_img.style.boxShadow="0px 0px 5px 2px #ccd0cf";
+    } else {
+        color_img.style.boxShadow="0px 0px 5px 2px #345269";
+    }
+});
+
+navBar.addEventListener("click", () => {
+    navBar.classList.toggle("menu");
+});
